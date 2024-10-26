39fcd4aa327a10997259dd213a5527f39a1ae257
Ignacio Cicero <ignaciocicero@gmail.com>
2024-10-26 12:19:37 +0100

    initial commit

---
+++ exercises/01_variables/variables3.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // TODO: Change the line below to fix the compiler error.
+    let x: i128=170141183460469231731687303715884105727;
+
+    println!("Number {x}");
+}
---
+++ exercises/02_functions/functions5.rs
@@ -0,0 +1,13 @@
+// TODO: Fix the function body without changing the signature.
+fn square(num: i32) -> i32 {
+    num * num
+    /*
+    1. Add the `return` keyword before `num * num;`
+    2. Remove the semicolon `;` after `num * num`
+    */
+}
+
+fn main() {
+    let answer = square(3);
+    println!("The square of 3 is {answer}");
+}
